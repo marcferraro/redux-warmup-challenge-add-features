04d57a9b15f6eb04dd5008c5095e11e8d9718c35
Marc <marc.ferraro1@gmail.com>
2021-03-08 10:48:04 -0600

    Delete Paintings and Default painting id

--- src/paintings/PaintingListItem.js
+++ src/paintings/PaintingListItem.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
-const PaintingListItem = ({ painting, selectPainting }) => {
+const PaintingListItem = ( {painting, selectPainting} ) => {
+  // console.log(painting, selectPainting)
   return (
     <div onClick={() => selectPainting(painting.id)} className="ui item">
       <span style={{ cursor: 'pointer' }}>{painting.title}</span>
     </div>
+    // <div>helllo</div>
   );
 };
 
